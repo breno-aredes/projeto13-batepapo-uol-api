89eb3d118836a19dc23f169268fa97af64d51277
breno-aredes <breno_mg10@hotmail.com>
2023-01-12 15:53:29 -0300

    conecta o servidor ao banco de dados mongo

--- src/app.js
+++ src/app.js
@@ -13,9 +13,18 @@ server.use(cors());
 server.use(express.json());
 //para usar mongoclient, conectado ao endereço
 const mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+let db;
 
 //connect é uma função do mongo que retorna uma promisse, podendo assim usar then e catch
-mongoClient.connect().then().catch();
+mongoClient
+  .connect()
+  .then(() => {
+    db = mongoClient.db("DATABASE_URL");
+    console.log("conectado o servidor ao banco de dados mongodb");
+  })
+  .catch(() => {
+    console.log("caminho não encontrado");
+  });
 
 //roda server na porta 5000
 server.listen(PORT);
