eecef0ad03650e2bc96581dbbccbcd6705da8633
breno-aredes <breno_mg10@hotmail.com>
2023-01-12 15:44:42 -0300

    mongodb instalado

--- src/app.js
+++ src/app.js
@@ -1,14 +1,21 @@
 import express from "express";
 import cors from "cors";
+import { MongoClient } from "mongodb";
 
 //cria porta para servidor
 const PORT = 5000;
+
 //cria o servidor
 const server = express();
 //libera acesso ao servidor
 server.use(cors());
 //utiliza json
 server.use(express.json());
+//para usar mongoclient, conectado ao endereço
+const mongoClient = new MongoClient("mongodb://127.0.0.1:27017");
+
+//connect é uma função do mongo que retorna uma promisse, podendo assim usar then e catch
+mongoClient.connect().then().catch();
 
 //roda server na porta 5000
 server.listen(PORT);
